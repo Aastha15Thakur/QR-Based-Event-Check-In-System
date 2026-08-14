5914d215118240083ec11d60c21cacfc337a3115
aasthacodes <glbaastha1506@gmail.com>
2026-08-14 06:57:35 +0530

    Fix production CORS

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,10 @@ Base.metadata.create_all(bind=engine)
 # Allow Next.js frontend to communicate with FastAPI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://qr-based-event-check-in-system-mu.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
